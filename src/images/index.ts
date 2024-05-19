938e2a628188041a10f7fc27a759ff8de7e6bf44
lasagna <arthurebenezer@aol.com>
2024-05-19 17:00:25 +0000

    fix: image, debugging

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,81 +1,81 @@
-import polaroid from './barbera2.JPG';
-import beach from './maaha1.PNG';
-import woodhouse from './maaha2.JPG';
-import light from './maaha3.JPG';
-import shore from './maaha4.JPG';
-import boat from './maaha5.JPG';
-import pool from './pool.JPG';
-import scene from './pool2.JPG';
-import coconut from './maaha17.JPG';
-import bereal from './bereal.JPG';
-import cup from './cup.JPG';
-import golf from './golf.JPG';
-import mmms from './mmms.JPG';
-import view from './view.JPG';
-import ipad from './ipad.JPG';
-import lamp from './lamp.JPG';
-import glide from './mauritius1.JPG';
-import forest from './mauritius2.JPG';
+// import polaroid from './barbera2';
+// import beach from './maaha1.PNG';
+// import woodhouse from './maaha2.JPG';
+// import light from './maaha3.JPG';
+// import shore from './maaha4.JPG';
+// import boat from './maaha5.JPG';
+// import pool from './pool.JPG';
+// import scene from './pool2.JPG';
+// import coconut from './maaha17.JPG';
+// import bereal from './bereal.jpg';
+// import cup from './cup.JPG';
+// import golf from './golf.JPG';
+// import mmms from './mmms.JPG';
+// import view from './view.JPG';
+// import ipad from './ipad.JPG';
+// import lamp from './lamp.JPG';
+// import glide from './mauritius1.JPG';
+// import forest from './mauritius2.JPG';
 import river from './mauritius4.jpg';
 import city from './mauritius5.jpg';
 import building from './mauritius6.jpg';
 import statue from './mauritius7.jpg';
 import sunset from './sunset.jpg';
-import lake from './lake.JPG';
+// import lake from './lake.JPG';
 import kfc from './kfc.jpg';
 
 export const Images = [
-  { img: glide, title: 'Grand Baie, Mauritius', alt: 'Picture taken in Mauritius, Africa' },
-  { img: shore, title: 'Beach Front, Ellembele District', alt: 'Picture taken in Ellembele, GH' },
-  { img: ipad, title: 'Haatso, Greater Accra', alt: 'Picture taken in Haatso, GH' },
-  {
-    img: forest,
-    title: 'Pamplemousses Botanical Garden, Mauritius',
-    alt: 'Picture taken in Mauritius, Africa',
-  },
+  // { img: glide, title: 'Grand Baie, Mauritius', alt: 'Picture taken in Mauritius, Africa' },
+  // { img: shore, title: 'Beach Front, Ellembele District', alt: 'Picture taken in Ellembele, GH' },
+  // { img: ipad, title: 'Haatso, Greater Accra', alt: 'Picture taken in Haatso, GH' },
+  // {
+  //   img: forest,
+  //   title: 'Pamplemousses Botanical Garden, Mauritius',
+  //   alt: 'Picture taken in Mauritius, Africa',
+  // },
   { img: city, title: 'Port Louis, Mauritius', alt: 'Picture taken in Mauritius, Africa' },
-  { img: lake, title: 'Lawra, Upper West', alt: 'Picture taken in Ghana, Africa' },
+  // { img: lake, title: 'Lawra, Upper West', alt: 'Picture taken in Ghana, Africa' },
   {
     img: statue,
     title: 'Mangal Mahadev - Statue of Shiva, Grand Bassin, Savanne',
     alt: 'Picture taken in Mauritius, Africa',
   },
   { img: river, title: 'Port Louis, Mauritius', alt: 'Picture taken in Mauritius, Africa' },
-  {
-    img: light,
-    title: 'Maaha Beach Resort, Ellembele District',
-    alt: 'Picture taken in Ellembele, GH',
-  },
-  { img: coconut, title: 'Ellembele District, Western', alt: 'Picture taken in Ellembele, GH' },
-  {
-    img: boat,
-    title: 'Maaha Beach Resort, Ellembele District',
-    alt: 'Picture taken in Ellembele, GH',
-  },
-  {
-    img: beach,
-    title: 'Maaha Beach Resort, Ellembele District',
-    alt: 'Picture taken in Ellembele, GH',
-  },
-  {
-    img: view,
-    title: 'Maaha Beach Resort, Ellembele District',
-    alt: 'Picture taken in Ellembele, GH',
-  },
+  // {
+  //   img: light,
+  //   title: 'Maaha Beach Resort, Ellembele District',
+  //   alt: 'Picture taken in Ellembele, GH',
+  // },
+  // { img: coconut, title: 'Ellembele District, Western', alt: 'Picture taken in Ellembele, GH' },
+  // {
+  //   img: boat,
+  //   title: 'Maaha Beach Resort, Ellembele District',
+  //   alt: 'Picture taken in Ellembele, GH',
+  // },
+  // {
+  //   img: beach,
+  //   title: 'Maaha Beach Resort, Ellembele District',
+  //   alt: 'Picture taken in Ellembele, GH',
+  // },
+  // {
+  //   img: view,
+  //   title: 'Maaha Beach Resort, Ellembele District',
+  //   alt: 'Picture taken in Ellembele, GH',
+  // },
   { img: kfc, title: 'KFC, East Legon', alt: 'Picture taken in Ghana, Africa' },
-  { img: pool, title: 'Damang, Western', alt: 'Picture of Sonia & Cassandra' },
-  { img: mmms, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
+  // { img: pool, title: 'Damang, Western', alt: 'Picture of Sonia & Cassandra' },
+  // { img: mmms, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
   { img: sunset, title: 'Jirapa, Upper West', alt: 'Picture taken in Ghana, Africa' },
-  { img: cup, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
-  { img: lamp, title: 'Haatso, Greater Accra', alt: 'Picture taken in Haatso, GH' },
+  // { img: cup, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
+  // { img: lamp, title: 'Haatso, Greater Accra', alt: 'Picture taken in Haatso, GH' },
   { img: building, title: 'Port Louis, Mauritius', alt: 'Picture taken in Mauritius, Africa' },
-  { img: golf, title: 'Damang Golf Park, Western', alt: 'Picture taken in Damang, GH' },
-  {
-    img: woodhouse,
-    title: 'Maaha Beach Resort, Ellembele District',
-    alt: 'Picture taken in Ellembele, GH',
-  },
-  { img: scene, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
-  { img: polaroid, title: 'Atlantic Tower, Accra', alt: "Picture of Winma's polaroid" },
-  { img: bereal, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
+  // { img: golf, title: 'Damang Golf Park, Western', alt: 'Picture taken in Damang, GH' },
+  //   {
+  //     img: woodhouse,
+  //     title: 'Maaha Beach Resort, Ellembele District',
+  //     alt: 'Picture taken in Ellembele, GH',
+  //   },
+  //   { img: scene, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
+  //   { img: polaroid, title: 'Atlantic Tower, Accra', alt: "Picture of Winma's polaroid" },
+  //   { img: bereal, title: 'Damang, Western', alt: 'Picture taken in Damang, GH' },
 ];
